1aa5c2526d05431775577053df0d9547c616f0be
Lucas Oliveira <l.oliveira21p@gmail.com>
2018-08-15 20:34:51 -0300

    Added GSAP

---
+++ main.js
@@ -0,0 +1,3 @@
+TweenMax.from("header",2, {opacity:0, ease: Power4.easeOut, marginLeft: 500 });
+TweenMax.staggerFrom("section",2, {opacity:0, ease: Power4.easeOut, marginLeft: 500,delay:2});
+TweenMax.staggerFrom("footer",2, {opacity:0, ease: Back.easeOut, marginLeft: 500,delay:3});
